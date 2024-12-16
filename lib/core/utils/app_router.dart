aa63aba9158b71da3433b534487e6aea2feca1bb
AshrafElManDoh <ashraftrezegeh@gmail.com>
2024-12-16 23:44:00 +0200

    create login view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:sakan/features/authentication/presentation/views/login_view.dart';
 import 'package:sakan/features/authentication/presentation/views/sign_up_view.dart';
 import 'package:sakan/features/authentication/presentation/views/student_or_owner_view.dart';
 import 'package:sakan/features/introduction/presentation/views/introduction_view.dart';
@@ -7,18 +8,25 @@ abstract class AppRouter {
   static const studentOrOwner = '/studentOrOwner';
   static const introductionView = '/';
   static const signUpView = '/signUp';
-  static final router = GoRouter(routes: [
-    GoRoute(
-      path: studentOrOwner,
-      builder: (context, state) => const StudentOrOwnerView(),
-    ),
-    GoRoute(
-      path: introductionView,
-      builder: (context, state) => const IntroductionView(),
-    ),
-    GoRoute(
-      path: signUpView,
-      builder: (context, state) => SignUpView(),
-    )
-  ]);
+  static const loginView = '/login';
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: studentOrOwner,
+        builder: (context, state) => const StudentOrOwnerView(),
+      ),
+      GoRoute(
+        path: introductionView,
+        builder: (context, state) => const IntroductionView(),
+      ),
+      GoRoute(
+        path: signUpView,
+        builder: (context, state) => SignUpView(),
+      ),
+      GoRoute(
+        path: loginView,
+        builder: (context, state) => LoginView(),
+      ),
+    ],
+  );
 }
--- lib/features/authentication/presentation/views/widgets/login_body.dart
+++ lib/features/authentication/presentation/views/widgets/login_body.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:sakan/core/utils/app_router.dart';
 import 'package:sakan/features/authentication/presentation/views/widgets/custom_break_line.dart';
 import 'package:sakan/features/authentication/presentation/views/widgets/custom_button.dart';
 import 'package:sakan/features/authentication/presentation/views/widgets/custom_logo_and_name.dart';
-import 'package:sakan/features/authentication/presentation/views/widgets/custom_password_text_field.dart';
 import 'package:sakan/features/authentication/presentation/views/widgets/custom_social.dart';
 import 'package:sakan/features/authentication/presentation/views/widgets/custom_text_field.dart';
 
-class SignUpBody extends StatelessWidget {
-  const SignUpBody({super.key});
+class LoginBody extends StatelessWidget {
+  const LoginBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return  Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
@@ -25,41 +22,32 @@ class SignUpBody extends StatelessWidget {
               height: 16,
             ),
             Text(
-              "Sign up",
+              "Login",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
-            SizedBox(
-              height: 16,
-            ),
-            CustomTextField(name: "Enter Name"),
             SizedBox(
               height: 16,
             ),
             CustomTextField(name: "Enter Email"),
             SizedBox(
               height: 16,
-            ),
-            CustomPasswordTextField(name: "Enter Password"),
-            SizedBox(
-              height: 16,
-            ),
-            CustomPasswordTextField(name: "Confirm Password"),
+            ),CustomTextField(name: "Enter Password"),
             SizedBox(
               height: 16,
             ),
             Row(
               children: [
                 Text(
-                  'Already have an account?',
+                  'Forget password?',
                   style: TextStyle(
                       color: Color(0xff4F545A), fontWeight: FontWeight.w500),
                 ),
                 TextButton(
                   onPressed: () {
-                    GoRouter.of(context).push(AppRouter.loginView);
+                    
                   },
                   child: Text(
-                    "Login",
+                    "Click here",
                     style: TextStyle(
                       fontWeight: FontWeight.w600,
                       fontSize: 14,
@@ -70,7 +58,7 @@ class SignUpBody extends StatelessWidget {
               ],
             ),
             SizedBox(height: 42,),
-            CustomButton(name: "Sign up"),
+            CustomButton(name: "Login"),
             SizedBox(height: 24,),
             CustomBreakLine(),
             SizedBox(height: 36,),
