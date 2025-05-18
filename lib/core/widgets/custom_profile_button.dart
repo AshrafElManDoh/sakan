523b29fba3564c0ad8d2093efa483c73d12ab0da
AshrafElManDoh <ashraftrezegeh@gmail.com>
2025-05-18 23:50:26 +0300

    add image picker package to pick an image from gallery

--- lib/core/widgets/custom_profile_button.dart
+++ lib/core/widgets/custom_profile_button.dart
@@ -2,21 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:sakan/core/utils/app_styles.dart';
 
 class CustomProfileButton extends StatelessWidget {
-  const CustomProfileButton({super.key, required this.title, required this.color});
+  const CustomProfileButton(
+      {super.key, required this.title, required this.color, this.onTap});
   final String title;
   final Color color;
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 13),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Text(
-        title,
-        style: AppStyles.stylesSemiBold13.copyWith(
-          color: Colors.white,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 13),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          title,
+          style: AppStyles.stylesSemiBold13.copyWith(
+            color: Colors.white,
+          ),
         ),
       ),
     );
--- lib/features/profile/presentation/views/widgets/custom_user_info.dart
+++ lib/features/profile/presentation/views/widgets/custom_user_info.dart
@@ -1,10 +1,35 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sakan/constants.dart';
 import 'package:sakan/core/utils/app_styles.dart';
 import 'package:sakan/core/widgets/custom_profile_button.dart';
 
-class CustomUserInfo extends StatelessWidget {
+class CustomUserInfo extends StatefulWidget {
   const CustomUserInfo({super.key});
+
+  @override
+  State<CustomUserInfo> createState() => _CustomUserInfoState();
+}
+
+class _CustomUserInfoState extends State<CustomUserInfo> {
+  File? _imageFile;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage() async {
+    final pickedImage = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      setState(
+        () {
+          _imageFile = File(pickedImage.path);
+        },
+      );
+    } else {
+      print("there is not a picture");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +40,9 @@ class CustomUserInfo extends StatelessWidget {
         children: [
           CircleAvatar(
             radius: 50,
-            backgroundImage: AssetImage("assets/icon/Oval.png"),
+            backgroundImage: _imageFile != null
+                ? FileImage(_imageFile!)
+                : AssetImage("assets/icon/Oval.png"),
           ),
           Column(
             children: [
@@ -38,6 +65,7 @@ class CustomUserInfo extends StatelessWidget {
               CustomProfileButton(
                 title: "Upload new picture",
                 color: ksecondaryColor,
+                onTap: _pickImage,
               )
             ],
           )
